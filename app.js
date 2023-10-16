48f3846c60c779c32ce21ac54ecc0d4aa3d60f39
kislay-singh-rajput <95602593+kislay2405@users.noreply.github.com>
2023-10-17 01:27:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+const express= require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https=require("https")
+
+
+const app= express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/",function(req,res){
+    res.sendFile( __dirname + "/index.html")
+})
+app.post("/",function(req,res){
+    const firstName=req.body.firstname;
+    const lastName=req.body.lastname;
+    const email=req.body.Email;  
+    const data = {
+        members: [
+            {
+            email_address: email ,
+            status: "subscribed", 
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastName
+            }
+            }
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+    const url="https://us13.api.mailchimp.com/3.0/lists/7059afc914";
+    const options={
+        method : "POST",
+        auth : "key:5252f4f3c2cf409969549ebddf7b03ef-us13"
+    }
+    const request=https.request(url,options,function (response) {
+        response.on("data",function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+});
+app.listen(3500,function(){
+    console.log("server port is 3500");
+})
